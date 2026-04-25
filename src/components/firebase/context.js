56bbd1f91e9178a883b2063c0bcaf2c7dcf0defa
undomiel <undomiel@undomielui-MacBookAir.local>
2026-04-25 16:47:49 +0900

    Export FirebaseContext as named export for index.js import

--- src/components/firebase/context.js
+++ src/components/firebase/context.js
@@ -1,12 +1,12 @@
 import React, { useContext } from 'react';
 
-const FirebaseContext = React.createContext(null);
+export const FirebaseContext = React.createContext(null);
 
 export const withFirebase = Component => props => (
-    <FirebaseContext.Consumer>
-      {firebase => <Component {...props} firebase={firebase} />}
-    </FirebaseContext.Consumer>
-  );
+  <FirebaseContext.Consumer>
+    {firebase => <Component {...props} firebase={firebase} />}
+  </FirebaseContext.Consumer>
+);
   
 export const useFirebase = () => useContext(FirebaseContext);
 
