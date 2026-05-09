6fb8204a470fd27d7464d82ce4c32bcea452ffc7
eeyore81 <eeyore81@github.com>
2026-05-09 16:47:08 +0900

    Use GAS endpoint automatically when REACT_APP_GAS_ENDPOINT is set

--- src/components/firebase/index.js
+++ src/components/firebase/index.js
@@ -2,9 +2,8 @@ import FirebaseContext, { withFirebase } from './context';
 import Firebase from './firebase';
 import GasFirebase from './gasProxy';
 
-const useGas = process.env.REACT_APP_USE_GAS === 'true';
 const gasEndpoint = process.env.REACT_APP_GAS_ENDPOINT;
-const service = useGas ? new GasFirebase(gasEndpoint) : new Firebase();
+const service = gasEndpoint ? new GasFirebase(gasEndpoint) : new Firebase();
 
 export default service;
 
